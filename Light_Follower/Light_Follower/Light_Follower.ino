cba245f83b95d71cd84e33a7dc1136ce05be97f4
Sandeep Patil <yes.sandeep@gmail.com>
2014-08-30 14:05:12 +0530

    Update Light_Follower.ino

--- Light_Follower/Light_Follower/Light_Follower.ino
+++ Light_Follower/Light_Follower/Light_Follower.ino
@@ -1,24 +1,24 @@
 /*
 
+The Explore Robo kit has 3 Light Sensors (LDR's) connected to arduino pins
+8(Left), 9(Middle) and 10(Right)
+
+Note that these inputs are from the sensor sheild.
+The sensor sheild has a comparator, and outputs a logical 1 whenever light falls
+on a particular sensor.
 
 Check the below link for connections and other details
 http://exploreembedded.com/wiki/index.php?title=Explore_Robo 
 
-Testing changes on GitHub
 
 */
-
-
-
 int MotorLeft[2] = {A0,A1};//Motor Pins
-int MotorRight[2] = {A2,A3};//Motor Pins	
-
-
-
+int MotorRight[2] = {A2,A3};//Motor Pins
+int Sensors[3]= {8,9,10}; //Sensor input (left, middle, right)
 void setup()
 {
 	MotorInit();
-	
+	SensorsInit();
 	
 	//Serial.begin(9600);
 	//Serial.println("Light Follower Robot");
@@ -28,11 +28,25 @@ void setup()
 void loop()
 { 
 	
-	
-	
+     if( digitalRead(Sensors[0])==1)
+        Robot_Left();
+     else if( digitalRead(Sensors[1])==1)
+        Robot_Forward();
+     else if( digitalRead(Sensors[2])==1)
+        Robot_Right();
+    	
 }
 
 
+//configure sensors as input
+void SensorsInit()
+{
+	int i;
+	for(i=0 ; i<3; i++)
+	{
+		pinMode(Sensors[i],INPUT);
+         }
+}
 
 
 //Initialize the motor
@@ -87,6 +101,5 @@ void Robot_Stop()
 	digitalWrite(MotorRight[0],0);
 	digitalWrite(MotorRight[1],0);
 }
-
 
 
